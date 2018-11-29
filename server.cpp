3dc118287c0b8b266267f30c9ce3085a4b131169
Muhammad Kamal Nadjieb <kamal.nadjieb@gmail.com>
2018-11-29 23:16:49 +0700

    set 95 as default value for parameter cv::IMWRITE_JPEG_QUALITY

--- server.cpp
+++ server.cpp
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
 
         std::vector<int> param(2);
         param[0] = cv::IMWRITE_JPEG_QUALITY;
-        param[1] = 80;//default(95) 0-100
+        param[1] = 95;//default(95) 0-100
         cv::imencode(".jpg", image, buff, param);
 
         return buff;
